c876c0fcb400e53eee2e05cb7c92b709e155c59e
Timothy Grant <timothyleegrant1995@gmail.com>
2025-05-26 01:05:43 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from "url";
+
+
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json())
+
+app.get('/', (req,res)=>res.send("this is my test! soieson;w"));
+
+app.get('/ree', (req,res)=>res.send("you passed"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+function function_declared(req, res)
+{
+    //const __dirname = path.dirname(__filename);
+    const file_path = path.join(__dirname, "package.json");
+    res.sendFile(file_path);
+}
+
+app.get('/function_callback', function_declared)
+
+app.listen(PORT, ()=>console.log("listening right now hahah"))
+
+//console.log(process.env.PORT)
